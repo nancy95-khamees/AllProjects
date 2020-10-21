2aea822a15d9788a1695b337b83fca2f85da9994
Nancy <nancy@tickerchart.com>
2020-10-21 12:01:46 +0300

    Multiple sence (navigation controller)

---
+++ Muultiple Sence/Muultiple Sence/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  Muultiple Sence
+//
+//  Created by Nancy on 21/10/2020.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = " Second View Controller "
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Muultiple Sence/Muultiple Sence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Muultiple Sence
+//
+//  Created by Nancy on 21/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "First View Controller"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
